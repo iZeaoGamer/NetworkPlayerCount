7c2175dd0f035e15d2c7c4254b9e4fb8330689f6
xxAROX <57589973+xxAROX@users.noreply.github.com>
2020-04-22 18:55:27 +0200

    Update Main.php

--- src/xxAROX/NetworkPlayerCount/Main.php
+++ src/xxAROX/NetworkPlayerCount/Main.php
@@ -14,39 +14,11 @@ use xxAROX\NetworkPlayerCount\task\StartTask;
  */
 class Main extends PluginBase
 {
-	private static $instance;
-	const PREFIX = "§eStimoMC §8» §7";
-	private $prefix = self::PREFIX;
 	public static $playerCount = 0;
 	public static $isQueryDone = TRUE;
 
-
-	public function onLoad(): void{
-		self::$instance = $this;
-	}
-
 	public function onEnable(): void{
-		$config = $this->getConfig();
-
-		if (!$config->exists("waterdog-config-path")) {
-			$config->set("waterdog-config-path", "/home/mcpe/Proxy/config.yml");
-		}
-		if (!$config->exists("server-address")) {
-			$config->set("server-address", "stimomc.de");
-		}
-		$config->save();
 		$this->getScheduler()->scheduleRepeatingTask(new StartTask(), 20 * 5);
-	}
-
-	public function onDisable(): void{
-	}
-
-	public function getPrefix(): string{
-		return $this->prefix;
-	}
-
-	public static function getInstance(): self{
-		return self::$instance;
 	}
 
 	public static function getNetworkPlayers(): int{
